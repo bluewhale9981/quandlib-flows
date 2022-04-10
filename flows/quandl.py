2e7853df3f10d112caed61ad35289fa33f3f2b16
Huy Tran <huy.tranquoc@asnet.com.vn>
2022-04-10 07:34:34 +0700

    Update a bit.

--- flows/quandl.py
+++ flows/quandl.py
@@ -8,12 +8,9 @@ from prestart import pre_start
 
 pre_start()
 
-# from tasks.quandl_daily import run_quandl_daily
+from tasks.quandl_daily import run_quandl_daily
 
-
-@task
-def do_something():
-    print('hello')
+RUN_TIME = '230pm'
 
 
 SLACK_URL: str = os.getenv('SLACK_URL')
@@ -21,29 +18,23 @@ SLACK_URL: str = os.getenv('SLACK_URL')
 if SLACK_URL is None:
     raise Exception('SLACK_URL is not set.')
 
-def register_quandl_daily(run_time: str) -> None:
-    run_config = LocalRun(
-        env={
-            'SLACK_URL': SLACK_URL,
-            'RUN_TIME': run_time
-        },
-    )
-    storage = Git(
-        repo='bluewhale9981/quanlib-flows',
-        flow_path='flows/quandl.py'
-    )
 
-    with Flow(
-        f'quandlib_daily_{run_time}',
-        run_config=run_config,
-        storage=storage,
-    ) as flow:
-        # run_quandl_daily(run_time=run_time)
-        do_something()
+run_config = LocalRun(
+    env={
+        'SLACK_URL': SLACK_URL,
+        'RUN_TIME': RUN_TIME
+    },
+)
+storage = Git(
+    repo='bluewhale9981/quanlib-flows',
+    flow_path='flows/quandl.py'
+)
 
-    flow.register(project_name='quandlib', labels=['quandl'])
+with Flow(
+    f'quandlib_daily_{RUN_TIME}',
+    run_config=run_config,
+    storage=storage,
+) as flow:
+    run_quandl_daily(run_time=RUN_TIME)
 
-
-if __name__ == '__main__':
-    register_quandl_daily('230pm')
-    register_quandl_daily('430am')
+flow.register(project_name='quandlib', labels=['quandl'])
