1347a5f0102039c19a2d5b2353d8b073dcf4d984
Huy Tran <huy.tranquoc@asnet.com.vn>
2022-04-16 23:30:09 +0700

    Push updates.

--- tasks/preprocess_fred.py
+++ tasks/preprocess_fred.py
@@ -4,24 +4,12 @@ This is the script to preprocess the data for FRED.
 from __future__ import absolute_import
 
 import os
-import pickle
 import typing
 import configparser
 
 import prefect
 import pandas as pd
-
-import sys
-from pathlib import Path  # if you haven't already done so
-file = Path(__file__).resolve()
-parent, root = file.parent, file.parents[1]
-sys.path.append(str(root))
-
-# Additionally remove the current file's directory from sys.path
-try:
-    sys.path.remove(str(parent))
-except ValueError: # Already removed
-    pass
+from prefect import task
 
 from tasks.base import BaseHandler
 from utils.storages import GoogleCloudStorage
