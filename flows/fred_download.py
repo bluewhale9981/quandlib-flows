167f3adb38d9fae314a2b6a8121f5a5adba4f832
Huy Tran <huy.tranquoc@asnet.com.vn>
2022-04-23 22:41:50 +0700

    Update FRED download.

--- flows/fred_download.py
+++ flows/fred_download.py
@@ -1,9 +1,4 @@
-import os
-import typing
-
 from prefect import Flow
-from prefect.run_configs import LocalRun
-from prefect.storage import Git
 
 import sys
 from pathlib import Path  # if you haven't already done so
@@ -19,30 +14,13 @@ except ValueError: # Already removed
 
 
 from tasks.download_fred import run_download_fred
-
-
-SLACK_URL: typing.Optional[str] = os.getenv('SLACK_URL')
-GOOGLE_APPLICATION_CREDENTIALS: typing.Optional[str] = os.getenv('GOOGLE_APPLICATION_CREDENTIALS')
-FRED_API_KEY: typing.Optional[str] = os.getenv('FRED_API_KEY')
-
-if SLACK_URL is None:
-    raise Exception('SLACK_URL is not set.')
-
-if GOOGLE_APPLICATION_CREDENTIALS is None:
-    raise Exception('GOOGLE_APPLICATION_CREDENTIALS is not set.')
+from flows.base import BaseFlow
 
 
 with Flow(
-    'download_fred',
-    run_config=LocalRun(
-        labels=['quandl'],
-        env={
-                'FRED_API_KEY': FRED_API_KEY,
-                'SLACK_URL': SLACK_URL,
-                'GOOGLE_APPLICATION_CREDENTIALS': GOOGLE_APPLICATION_CREDENTIALS,
-            }
-        ),
-    storage=Git(repo='bluewhale9981/quandlib-flows', flow_path='flows/fred_download.py'),
+    'fred_download',
+    run_config=BaseFlow.get_local_run(),
+    storage=BaseFlow.get_storage(flow_file='fred_download.py'),
 ) as flow:
     run_download_fred()
 
